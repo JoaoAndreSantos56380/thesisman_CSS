e921b8a6ef3e1d52a55a7357918f9745cc6d50af
JPBotelho <jpbotelho.costa@gmail.com>
2024-04-01 17:58:12 +0100

    Renamed ThesisProposal to ThesisDefense

--- src/main/java/pt/ul/fc/css/projeto/entities/FinalPresentation.java
+++ src/main/java/pt/ul/fc/css/projeto/entities/FinalPresentation.java
@@ -1,17 +1,16 @@
 package pt.ul.fc.css.projeto.entities;
 
-import org.springframework.lang.NonNull;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import org.springframework.lang.NonNull;
 
 @Entity
-public class FinalPresentation extends ProposalPresentation{
-	//associacao many-to-one
-	@NonNull
-	@ManyToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "fk_president_id", referencedColumnName = "id")
-	private Professor president;
+public class FinalPresentation extends ThesisDefense {
+  // associacao many-to-one
+  @NonNull
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "fk_president_id", referencedColumnName = "id")
+  private Professor president;
 }
---
+++ src/main/java/pt/ul/fc/css/projeto/entities/ThesisDefense.java
@@ -0,0 +1,53 @@
+package pt.ul.fc.css.projeto.entities;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.Date;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
+@Entity
+public class ThesisDefense {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long id;
+
+  @NonNull
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "fk_thesisExecution_id", referencedColumnName = "id")
+  private ThesisExecution thesisExecution;
+
+  /*
+   * @NonNull
+   *
+   * @Lob
+   * private File manuscriptFile;
+   */
+
+  @Nullable private double grade;
+
+  @NonNull private String location;
+
+  @NonNull private Date time;
+
+  public void assignGrade(int grade) {
+    this.grade = grade;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public ThesisExecution getThesisExecution() {
+    return thesisExecution;
+  }
+
+  public double getGrade() {
+    return grade;
+  }
+}
