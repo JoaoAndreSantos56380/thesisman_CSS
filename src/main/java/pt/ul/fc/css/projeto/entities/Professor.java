d9b10dd3d68c3774870ba2a6f1654344cfbdbd91
JPBotelho <jpbotelho.costa@gmail.com>
2024-04-01 17:56:15 +0100

    Professor.java: removed isAdmin
    Masters table with contain their coordinator's ID. No need to store it again

--- src/main/java/pt/ul/fc/css/projeto/entities/Professor.java
+++ src/main/java/pt/ul/fc/css/projeto/entities/Professor.java
@@ -1,7 +1,5 @@
 package pt.ul.fc.css.projeto.entities;
 
-import org.springframework.lang.NonNull;
-
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
@@ -9,18 +7,9 @@ import jakarta.persistence.Entity;
 @DiscriminatorValue("TEACHER")
 public class Professor extends User {
 
-	public Professor(String username, String password, String name, Boolean isAdmin) {
-		super(username, password, name);
-		this.isAdmin = isAdmin;
-	}
+  public Professor(String username, String password, String name) {
+    super(username, password, name);
+  }
 
-	public Professor() {
-	}
-
-	@NonNull
-	private boolean isAdmin;
-
-	public boolean getIsAdmin(){
-		return isAdmin;
-	}
+  public Professor() {}
 }
