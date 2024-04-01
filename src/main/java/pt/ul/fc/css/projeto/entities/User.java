4cbfaf409fce1791af0111db22b61c30cd55542e
JPBotelho <jpbotelho.costa@gmail.com>
2024-04-01 17:51:18 +0100

    Removed setID in User.java
    Field is generated by JPA and is meant to be readonly

--- src/main/java/pt/ul/fc/css/projeto/entities/User.java
+++ src/main/java/pt/ul/fc/css/projeto/entities/User.java
@@ -1,60 +1,51 @@
 package pt.ul.fc.css.projeto.entities;
 
-import org.springframework.lang.NonNull;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import org.springframework.lang.NonNull;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public abstract class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long id;
 
-	@NonNull
-	private String username;
+  @NonNull private String username;
 
-	@NonNull
-	private String password;
+  @NonNull private String password;
 
-	@NonNull
-	private String name;
+  @NonNull private String name;
 
-	public User(@NonNull String username, @NonNull String password, @NonNull String name) {
-		this.username = username;
-		this.password = password;
-		this.name = name;
-	}
+  public User(@NonNull String username, @NonNull String password, @NonNull String name) {
+    this.username = username;
+    this.password = password;
+    this.name = name;
+  }
 
-	public User() {
-	}
+  public User() {}
 
-	public long getId() {
-		return id;
-	}
+  public long getId() {
+    return id;
+  }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+  public String getName() {
+    return username;
+  }
 
-	public String getName() {
-		return username;
-	}
+  public void setName(String username) {
+    this.username = username;
+  }
 
-	public void setName(String username) {
-		this.username = username;
-	}
+  public String getPassword() {
+    return password;
+  }
 
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+  public void setPassword(String password) {
+    this.password = password;
+  }
 }
