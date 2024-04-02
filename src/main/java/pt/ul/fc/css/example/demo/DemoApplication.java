9a6de7f35859935428061903301a782d753b0685
JPBotelho <jpbotelho.costa@gmail.com>
2024-04-02 23:41:25 +0100

    Changed DemoApp to use 'submitter' and not 2 'advisors'

--- src/main/java/pt/ul/fc/css/example/demo/DemoApplication.java
+++ src/main/java/pt/ul/fc/css/example/demo/DemoApplication.java
@@ -2,14 +2,12 @@ package pt.ul.fc.css.example.demo;
 
 import java.util.ArrayList;
 import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import pt.ul.fc.css.example.demo.entities.Application;
 import pt.ul.fc.css.example.demo.entities.Consultant;
 import pt.ul.fc.css.example.demo.entities.DissertationTopic;
@@ -29,79 +27,92 @@ import pt.ul.fc.css.example.demo.repositories.UserRepository;
 @SpringBootApplication
 public class DemoApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+  private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 
-	@Bean
-	public CommandLineRunner demo(
-			UserRepository userRepository, MastersRepository mastersRepository,
-			DissertationTopicRepository dissertationTopicRepository,
-			ThesisExecutionRepository thesisExecutionRepository, DefenseRepository defenseRepository,
-			ApplicationRepository applicationRepository) {
-		return (args) -> {
-			// criar professor
-			Professor cr7Professor = new Professor("cr7", "password", "cristiano ronaldo");
-			Professor rq7Professor = new Professor("rq7", "password", "ricardo quaresma");
-			Professor presiJuri = new Professor("pjuri", "password", "presi juri");
-			Consultant consultant = new Consultant("consultant@mail.com", "password", "jorge mendes", " sumol");
+  @Bean
+  public CommandLineRunner demo(
+      UserRepository userRepository,
+      MastersRepository mastersRepository,
+      DissertationTopicRepository dissertationTopicRepository,
+      ThesisExecutionRepository thesisExecutionRepository,
+      DefenseRepository defenseRepository,
+      ApplicationRepository applicationRepository) {
+    return (args) -> {
+      // criar professor
+      Professor cr7Professor = new Professor("cr7", "password", "cristiano ronaldo");
+      Professor rq7Professor = new Professor("rq7", "password", "ricardo quaresma");
+      Professor presiJuri = new Professor("pjuri", "password", "presi juri");
+      Consultant consultant =
+          new Consultant("consultant@mail.com", "password", "jorge mendes", " sumol");
 
-			// criar mestrado com esse prof a coordenador
-			Masters theBestMaster = new Masters("the best", cr7Professor);
-			Masters master2 = new Masters("mustang", rq7Professor);
+      // criar mestrado com esse prof a coordenador
+      Masters theBestMaster = new Masters("the best", cr7Professor);
+      Masters master2 = new Masters("mustang", rq7Professor);
 
-			Student mCurieStudent = new Student("radiante", "password", "Marie Curie", 1903, 19.99, theBestMaster);
+      Student mCurieStudent =
+          new Student("radiante", "password", "Marie Curie", 1903, 19.99, theBestMaster);
 
-			ArrayList<Masters> mastersArr = new ArrayList<Masters>();
-			mastersArr.add(theBestMaster);
+      ArrayList<Masters> mastersArr = new ArrayList<Masters>();
+      mastersArr.add(theBestMaster);
 
-			DissertationTopic dissertationTopicFutebolRadiante = new DissertationTopic("Futebol radiante", "description", 99999, rq7Professor, null, mastersArr);
+      DissertationTopic dissertationTopicFutebolRadiante =
+          new DissertationTopic("Futebol radiante", "description", 99999, rq7Professor, mastersArr);
 
-			userRepository.save(consultant);
-			userRepository.save(cr7Professor);
-			userRepository.save(rq7Professor);
-			mastersRepository.save(theBestMaster);
-			userRepository.save(mCurieStudent);
-			mastersRepository.save(master2);
+      userRepository.save(consultant);
+      userRepository.save(cr7Professor);
+      userRepository.save(rq7Professor);
+      mastersRepository.save(theBestMaster);
+      userRepository.save(mCurieStudent);
+      mastersRepository.save(master2);
 
-			dissertationTopicRepository.save(dissertationTopicFutebolRadiante);
+      dissertationTopicRepository.save(dissertationTopicFutebolRadiante);
 
-			ThesisExecution thesis = new ThesisExecution(mCurieStudent, dissertationTopicFutebolRadiante, new Date(), 20);
+      ThesisExecution thesis =
+          new ThesisExecution(mCurieStudent, dissertationTopicFutebolRadiante, new Date(), 20);
 
-			thesisExecutionRepository.save(thesis);
+      thesisExecutionRepository.save(thesis);
 
-			ThesisDefense defense = new ThesisDefense(thesis, "1.1.1", new Date(), 20);
+      ThesisDefense defense = new ThesisDefense(thesis, "1.1.1", new Date(), 20);
 
-			FinalDefense finalDefense = new FinalDefense(thesis, "1.1.1", new Date(), 20, presiJuri);
+      FinalDefense finalDefense = new FinalDefense(thesis, "1.1.1", new Date(), 20, presiJuri);
 
-			defenseRepository.save(defense);
+      defenseRepository.save(defense);
 
-			defenseRepository.save(finalDefense);
+      defenseRepository.save(finalDefense);
 
-			Application application = new Application(mCurieStudent, dissertationTopicFutebolRadiante);
+      Application application = new Application(mCurieStudent, dissertationTopicFutebolRadiante);
 
-			applicationRepository.save(application);
+      applicationRepository.save(application);
 
-			/* Masters master3 = new Masters("Ingles", cr7Professor); */
-			// Masters master = new Masters("Matematica", prof3);
+      /* Masters master3 = new Masters("Ingles", cr7Professor); */
+      // Masters master = new Masters("Matematica", prof3);
 
-			/* ArrayList<Masters> mastersList = new ArrayList<>(); */
-			// mastersList.add(master);
-			/* mastersList.add(master2); */
+      /* ArrayList<Masters> mastersList = new ArrayList<>(); */
+      // mastersList.add(master);
+      /* mastersList.add(master2); */
 
-			// cria um DissertationTopic
-			/* DissertationTopic topic = new DissertationTopic("Biologia", "Memo bom", 1231.446, cr7Professor, null, null);
-			DissertationTopic topic2 = new DissertationTopic("TREX", "Memo bom", 1231.446, cr7Professor, null, null);
-			DissertationTopic topic3 = new DissertationTopic("TREX", "Memo bom", 1231.446, cr7Professor, null, null); */
+      // cria um DissertationTopic
+      /*
+       * DissertationTopic topic = new DissertationTopic("Biologia", "Memo bom",
+       * 1231.446, cr7Professor, null, null);
+       * DissertationTopic topic2 = new DissertationTopic("TREX", "Memo bom",
+       * 1231.446, cr7Professor, null, null);
+       * DissertationTopic topic3 = new DissertationTopic("TREX", "Memo bom",
+       * 1231.446, cr7Professor, null, null);
+       */
 
-			// mastersRepository.save(master);
-			// userRepository.save(cr7Professor);
-			/* dissertationTopicRepository.save(topic);
-			dissertationTopicRepository.save(topic2);
-			dissertationTopicRepository.save(topic3); */
+      // mastersRepository.save(master);
+      // userRepository.save(cr7Professor);
+      /*
+       * dissertationTopicRepository.save(topic);
+       * dissertationTopicRepository.save(topic2);
+       * dissertationTopicRepository.save(topic3);
+       */
 
-		};
-	}
+    };
+  }
 }
