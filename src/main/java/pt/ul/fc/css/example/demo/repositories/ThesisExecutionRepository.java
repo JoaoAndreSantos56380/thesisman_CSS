c9fbaddee3bff263d20100eeb984ed0a8de01980
JPBotelho <jpbotelho.costa@gmail.com>
2024-04-02 22:54:34 +0100

    ThesisExecutionRepo: +findByStudent

--- src/main/java/pt/ul/fc/css/example/demo/repositories/ThesisExecutionRepository.java
+++ src/main/java/pt/ul/fc/css/example/demo/repositories/ThesisExecutionRepository.java
@@ -1,18 +1,13 @@
 package pt.ul.fc.css.example.demo.repositories;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
+import pt.ul.fc.css.example.demo.entities.Student;
 import pt.ul.fc.css.example.demo.entities.ThesisExecution;
 
-public interface ThesisExecutionRepository extends JpaRepository<ThesisExecution, Long>{
-	/* @Query("SELECT a FROM Master a WHERE a.name LIKE %:q% ")
-    List<ThesisExecution> findByName(@Param("q") String q);
-
-    public void addThesis(ThesisExecution thesis);
-
-    public void removeThesis(ThesisExecution thesis); */
+public interface ThesisExecutionRepository extends JpaRepository<ThesisExecution, Long> {
+  @Query("SELECT t FROM ThesisExecution t WHERE t.student = :student")
+  List<ThesisExecution> findByStudent(@Param("student") Student student);
 }
