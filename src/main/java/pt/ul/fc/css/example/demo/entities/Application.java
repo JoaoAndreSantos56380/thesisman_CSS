b00f959a754e00f412d1796ffc3889abfb4d99ef
JPBotelho <jpbotelho.costa@gmail.com>
2024-04-03 00:32:32 +0100

    Application: +getId, +getStudent, +getTopic

--- src/main/java/pt/ul/fc/css/example/demo/entities/Application.java
+++ src/main/java/pt/ul/fc/css/example/demo/entities/Application.java
@@ -1,7 +1,6 @@
 package pt.ul.fc.css.example.demo.entities;
 
 import io.micrometer.common.lang.NonNull;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,18 +16,30 @@ public class Application {
 
   @NonNull
   // associacao one-to-many
-  @ManyToOne //(cascade = CascadeType.ALL)
+  @ManyToOne // (cascade = CascadeType.ALL)
   @JoinColumn(name = "fk_student_id", referencedColumnName = "id")
   private Student student;
 
   // associacao one-to-one
   @NonNull
-  @ManyToOne //(cascade = CascadeType.ALL)
+  @ManyToOne // (cascade = CascadeType.ALL)
   @JoinColumn(name = "fk_dissertation_topic_id", referencedColumnName = "id")
   private DissertationTopic topic;
 
   public Application(Student student, DissertationTopic topic) {
     this.student = student;
     this.topic = topic;
+  }
+
+  public Long getId() {
+    return this.id;
+  }
+
+  public Student getStudent() {
+    return this.student;
+  }
+
+  public DissertationTopic getTopic() {
+    return this.topic;
   }
 }
