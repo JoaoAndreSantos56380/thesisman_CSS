ff4a06ff995b17a92202aa7d0f76c35abfded0b5
fc56380 <fc56380@alunos.fc.ul.pt>
2024-04-02 22:09:10 +0100

    added queries

--- src/main/java/pt/ul/fc/css/example/demo/repositories/UserRepository.java
+++ src/main/java/pt/ul/fc/css/example/demo/repositories/UserRepository.java
@@ -10,43 +10,14 @@ import pt.ul.fc.css.example.demo.entities.AppUser;
 
 public interface UserRepository extends JpaRepository<AppUser, Long> {
 
-  @Query("SELECT a FROM AppUser a WHERE a.name LIKE %:q% ")
-  List<AppUser> findByName(@Param("q") String q);
+	@Query("SELECT a FROM AppUser a WHERE a.name LIKE %:q% ")
+	List<AppUser> findByName(@Param("q") String q);
 
-  @Transactional
-  @Modifying
-  @Query("DELETE FROM AppUser u WHERE u.id = :userId")
-  void removeUser(@Param("userId") Long userId);
+	@Query("SELECT a FROM AppUser a WHERE a.username LIKE %:q% ")
+	List<AppUser> findByUserName(@Param("q") String q);
 
-  @Transactional
-  @Modifying
-  @Query("update Student s set s.studentNumber = :studentNumber where s.id = :userId")
-  void updateStudentNumber(
-      @Param("userId") Long userId, @Param("studentNumber") Integer studentNumber);
+	/* @Query("SELECT a FROM AppUser a WHERE a.studentNumber = :number ")
+	List<AppUser> findByStudentNumber(@Param("number") int number); */
 
-  /*
-  @Modifying
-
-  @Query("update Student s set s.masters = :masters where s.id = :userId")
-  void updateStudentMasters(@Param("userId") Integer userId, @Param ("masters") Masters masters);
-
-  @Modifying
-
-  @Query("update Student s set s.averageGrade = :averageGrade where s.id = :userId"
-   )
-  void updateStudentGrades(@Param("userId") Integer userId, @Param
-   ("averageGrade") Float averageGrade);
-
-  @Modifying
-
-  @Query("update Professor p set p.emailField = :emailField where p.id = :userId"
-   )
-  void updateProfessorEmail(@Param("userId") Integer userId, @Param
-   ("emailField") String emailField);
-
-  @Modifying
-
-  @Query("update Consultant c set c.company = :company where c.id = :userId")
-   void updateConsultantCompany(@Param("userId") Integer userId, @Param ("company") String company); */
 
 }
