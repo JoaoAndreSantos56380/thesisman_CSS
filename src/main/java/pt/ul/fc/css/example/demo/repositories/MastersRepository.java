114465f5df0f2fea55d0de7777caf073ec12b3db
fc56380 <fc56380@alunos.fc.ul.pt>
2024-04-02 15:46:36 +0100

    adding masters repository

---
+++ src/main/java/pt/ul/fc/css/example/demo/repositories/MastersRepository.java
@@ -0,0 +1,27 @@
+package pt.ul.fc.css.example.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import jakarta.transaction.Transactional;
+import pt.ul.fc.css.example.demo.entities.Masters;
+import pt.ul.fc.css.example.demo.entities.Professor;
+
+import java.util.List;
+
+public interface MastersRepository extends JpaRepository<Masters, Long> {
+
+	// Custom query to find Masters by name
+	@Transactional
+	@Modifying
+	@Query("SELECT m FROM Masters m WHERE m.name LIKE %:q% ")
+	List<Masters> findByName(@Param("q") String name);
+
+	// Custom query to find Masters by coordinator
+	@Transactional
+	@Modifying
+	@Query("SELECT m FROM Masters m WHERE m.coordinator.id = :userId")
+	List<Masters> findByCoordinator(@Param(":userId") Professor coordinator);
+}
