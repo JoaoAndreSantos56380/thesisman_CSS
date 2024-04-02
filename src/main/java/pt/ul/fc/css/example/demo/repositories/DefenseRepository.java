8b7e0fb88d4bf2a27e28082345445af19d662003
JPBotelho <jpbotelho.costa@gmail.com>
2024-04-02 23:13:16 +0100

    DefenseRepo: +findByStudent, +findByLocation, +findByTime

--- src/main/java/pt/ul/fc/css/example/demo/repositories/DefenseRepository.java
+++ src/main/java/pt/ul/fc/css/example/demo/repositories/DefenseRepository.java
@@ -1,18 +1,20 @@
 package pt.ul.fc.css.example.demo.repositories;
 
+import java.util.Date;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
+import pt.ul.fc.css.example.demo.entities.Student;
 import pt.ul.fc.css.example.demo.entities.ThesisDefense;
 
-public interface DefenseRepository extends JpaRepository<ThesisDefense, Long>{
-	/* @Query("SELECT a FROM Masters a WHERE a.name LIKE %:q% ")
-    List<ThesisDefense> findByName(@Param("q") String q);
+public interface DefenseRepository extends JpaRepository<ThesisDefense, Long> {
+  @Query("SELECT d FROM ThesisDefense d WHERE d.thesisExecution.student = :student")
+  List<ThesisDefense> findByStudent(@Param("student") Student student);
 
-    public void addProposalPresentation(ThesisDefense proposal);
+  @Query("SELECT d FROM ThesisDefense d WHERE d.location = :location")
+  List<ThesisDefense> findByLocation(@Param("location") String location);
 
-    public void removeProposalPresentation(ThesisDefense proposal); */
+  @Query("SELECT d FROM ThesisDefense d WHERE d.time = :time")
+  List<ThesisDefense> findByTime(@Param("time") Date time);
 }
