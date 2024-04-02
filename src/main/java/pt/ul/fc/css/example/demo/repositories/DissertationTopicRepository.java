4338a9bef76699cc9151c75ca19264e677e27a8b
JPBotelho <jpbotelho.costa@gmail.com>
2024-04-02 22:47:04 +0100

    Added more queries to DissTopicRepo
    +findByInternalAdvisor, +findByExternalAdvisor, +findByCompatibleMasters, +findByTitle

--- src/main/java/pt/ul/fc/css/example/demo/repositories/DissertationTopicRepository.java
+++ src/main/java/pt/ul/fc/css/example/demo/repositories/DissertationTopicRepository.java
@@ -1,33 +1,25 @@
 package pt.ul.fc.css.example.demo.repositories;
 
-import jakarta.transaction.Transactional;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import pt.ul.fc.css.example.demo.entities.AppUser;
+import pt.ul.fc.css.example.demo.entities.Consultant;
 import pt.ul.fc.css.example.demo.entities.DissertationTopic;
+import pt.ul.fc.css.example.demo.entities.Masters;
+import pt.ul.fc.css.example.demo.entities.Professor;
 
 public interface DissertationTopicRepository extends JpaRepository<DissertationTopic, Long> {
 
-  /* @Transactional
-  @Modifying
+  @Query("SELECT d FROM DissertationTopic d WHERE d.internalAdvisor = :advisor")
+  List<DissertationTopic> findByInternalAdvisor(@Param("advisor") Professor advisor);
+
+  @Query("SELECT d FROM DissertationTopic d WHERE d.externalAdvisor = :advisor")
+  List<DissertationTopic> findByExternalAdvisor(@Param("advisor") Consultant advisor);
+
+  @Query("SELECT d FROM DissertationTopic d JOIN d.compatibleMasters m WHERE m = :masters")
+  List<DissertationTopic> findByCompatibleMasters(@Param("masters") Masters masters);
+
   @Query("SELECT dt FROM DissertationTopic dt WHERE dt.title LIKE %:q% ")
-  List<DissertationTopic> findByTitle(@Param("q") String q); */
-
-
-  /*
-  @Transactional
-  @Modifying
-  @Query("SELECT * FROM DissertationTopic d WHERE d.id = :dId")
-  void findId(@Param("dId") Long dId);
-/**
-  @Transactional
-  @Modifying
-  @Query("update Student s set s.studentNumber = :studentNumber where s.id = :userId")
-  void updateStudentNumber(
-      @Param("userId") Long userId, @Param("studentNumber") Integer studentNumber);
-  */
-
+  List<DissertationTopic> findByTitle(@Param("q") String q);
 }
