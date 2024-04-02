039866a9311809240ad292e1dfede952a8dcaf1b
JPBotelho <jpbotelho.costa@gmail.com>
2024-04-02 22:32:56 +0100

    MastersRepo: +findByName, +findByCoordinator

--- src/main/java/pt/ul/fc/css/example/demo/repositories/MastersRepository.java
+++ src/main/java/pt/ul/fc/css/example/demo/repositories/MastersRepository.java
@@ -1,31 +1,17 @@
 package pt.ul.fc.css.example.demo.repositories;
 
-import jakarta.transaction.Transactional;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import pt.ul.fc.css.example.demo.entities.Masters;
+import pt.ul.fc.css.example.demo.entities.Professor;
 
 public interface MastersRepository extends JpaRepository<Masters, Long> {
 
-  /* // Custom query to find Masters by name
-  @Transactional
-  @Modifying
-  @Query("SELECT m FROM Masters m WHERE m.name LIKE %:q% ")
-  List<Masters> findByName(@Param("q") String name);
+  @Query("SELECT a FROM Masters a WHERE a.name LIKE %:q% ")
+  List<Masters> findByName(@Param("q") String q);
 
-  // Custom query to find Masters by coordinator
-  @Transactional
-  @Modifying
-  @Query("SELECT m FROM Masters m WHERE m.coordinator.id = :coordinatorId")
-  List<Masters> findByCoordinator(@Param("coordinatorId") Long coordinatorId);
-
-  	@Query("SELECT a FROM Master a WHERE a.name LIKE %:q% ")
-	List<Masters> findByName(@Param("q") String q);
-
-	public void addMasters(Masters masters);
-
-	public void removeMasters(Masters masters); */
+  @Query("SELECT m FROM Masters m WHERE m.coordinator = :professor")
+  List<Masters> findByCoordinator(@Param("professor") Professor professor);
 }
