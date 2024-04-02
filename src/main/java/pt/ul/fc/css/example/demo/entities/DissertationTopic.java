2e826671d2a49238030636498a1a3fc63e6b7423
JPBotelho <jpbotelho.costa@gmail.com>
2024-04-02 23:34:51 +0100

    DissertationTopic now has a 'submitter' instead of internal/external advisor

--- src/main/java/pt/ul/fc/css/example/demo/entities/DissertationTopic.java
+++ src/main/java/pt/ul/fc/css/example/demo/entities/DissertationTopic.java
@@ -2,7 +2,6 @@ package pt.ul.fc.css.example.demo.entities;
 
 import io.micrometer.common.lang.NonNull;
 import io.micrometer.common.lang.Nullable;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,26 +19,22 @@ public class DissertationTopic {
   @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private Long id;
 
+  @NonNull private DissertationTopicType type;
+
   @NonNull private String title;
 
   @NonNull private String description;
 
   @NonNull private double salary;
 
-  // associacao many-to-one, este cascade da erro
-  @NonNull
-  @ManyToOne //(cascade = CascadeType.PERSIST)
-  @JoinColumn(name = "fk_professor_id", referencedColumnName = "id")
-  private Professor internalAdvisor;
-
   // associacao many-to-one
   @Nullable
-  @ManyToOne //(cascade = CascadeType.PERSIST)
+  @ManyToOne // (cascade = CascadeType.PERSIST)
   @JoinColumn(name = "fk_consultant_id", referencedColumnName = "id")
-  private Consultant externalAdvisor;
+  private AppUser submitter;
 
   @Nullable
-  @ManyToMany //(cascade = CascadeType.PERSIST)
+  @ManyToMany // (cascade = CascadeType.PERSIST)
   @JoinTable(
       name = "dissertation_topic_compatible_masters",
       joinColumns = @JoinColumn(name = "dissertation_topic_id"),
@@ -67,24 +62,31 @@ public class DissertationTopic {
     this.description = description;
   }
 
-  public Consultant getConsultant() {
-    return this.externalAdvisor;
-  }
-
   public List<Masters> getCompatibleMasters() {
     return this.compatibleMasters;
   }
 
   public DissertationTopic() {}
 
-  public DissertationTopic(@NonNull String title, @NonNull String description, @NonNull double salary, @NonNull Professor internalAdvisor, @Nullable Consultant externalAdvisor, @Nullable ArrayList<Masters> compatibleMasters) {
+  public DissertationTopic(
+      @NonNull String title,
+      @NonNull String description,
+      @NonNull double salary,
+      @NonNull AppUser submitter,
+      @Nullable ArrayList<Masters> compatibleMasters) {
     this.title = title;
     this.description = description;
     this.salary = salary;
-    this.internalAdvisor = internalAdvisor;
-    this.externalAdvisor = externalAdvisor;
+    this.submitter = submitter;
     this.compatibleMasters = compatibleMasters;
+
+    if (submitter.getClass() == Professor.class) {
+      this.type = DissertationTopicType.DISSERTATION;
+    } else {
+      this.type = DissertationTopicType.PROJECT;
+    }
   }
+
   @Override
   public String toString() {
     return "DissertationTopic["
