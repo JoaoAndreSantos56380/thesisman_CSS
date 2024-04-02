57977c3980d48d00eb5f39b1128c224cb7e72a8c
fc56380 <fc56380@alunos.fc.ul.pt>
2024-04-02 10:33:40 +0100

    changes to add AppUsers

--- src/main/java/pt/ul/fc/css/example/demo/DemoApplication.java
+++ src/main/java/pt/ul/fc/css/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package pt.ul.fc.css.example.demo;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -7,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import pt.ul.fc.css.example.demo.entities.AppUser;
 import pt.ul.fc.css.example.demo.entities.Author;
+import pt.ul.fc.css.example.demo.entities.Masters;
+import pt.ul.fc.css.example.demo.entities.Student;
 import pt.ul.fc.css.example.demo.repositories.AuthorRepository;
+import pt.ul.fc.css.example.demo.repositories.UserRepository;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -20,7 +26,7 @@ public class DemoApplication {
     }
 
     @Bean
-    public CommandLineRunner demo(AuthorRepository repository) {
+    public CommandLineRunner demo(AuthorRepository repository, UserRepository user) {
         return (args) -> {
             // save a few customers
             repository.save(new Author("Jack", "Bauer"));
@@ -55,6 +61,18 @@ public class DemoApplication {
             // log.info(bauer.toString());
             // }
             log.info("");
+
+			Student student = new Student("username", "password", "name", 56380, 13.48, null);
+			System.out.println(student);
+			user.save(student);
+			List<AppUser> students = user.findByName("name");
+			for (AppUser appUser : students) {
+				System.out.println("appUser:");
+				System.out.println(appUser.getName());
+				System.out.println(appUser.getId());
+				System.out.println(appUser.getPassword());
+			}
+			System.out.println("hello");
         };
     }
 
