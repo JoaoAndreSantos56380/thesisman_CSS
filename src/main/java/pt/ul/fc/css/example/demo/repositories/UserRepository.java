5c9034ed850b9cb72713263e982702a73cd95cc0
fc56380 <fc56380@alunos.fc.ul.pt>
2024-04-02 16:11:05 +0100

    removeUser form user repository working

--- src/main/java/pt/ul/fc/css/example/demo/repositories/UserRepository.java
+++ src/main/java/pt/ul/fc/css/example/demo/repositories/UserRepository.java
@@ -14,54 +14,39 @@ public interface UserRepository extends JpaRepository<AppUser, Long> {
 	@Query("SELECT a FROM AppUser a WHERE a.name LIKE %:q% ")
 	List<AppUser> findByName(@Param("q") String q);
 
+	@Transactional
+	@Modifying
+	@Query("DELETE FROM AppUser u WHERE u.id = :userId")
+	void removeUser(@Param("userId") Long userId);
+
+	@Transactional
+	@Modifying
+	@Query("update Student s set s.studentNumber = :studentNumber where s.id = :userId")
+	void updateStudentNumber(@Param("userId") Integer userId, @Param ("studentNumber") Integer studentNumber);
+
 	/*
-	 * @Transactional
-	 *
-	 * @Modifying
-	 *
-	 * @Query("INSERT INTO User (username, password, name) VALUES (:username, :password, :name)"
-	 * )
-	 * void addUser(@Param("username") String username, @Param("password") String
-	 * password, @Param("name") String name);
-	 *
-	 * @Transactional
-	 *
-	 * @Modifying
-	 *
-	 * @Query("DELETE FROM User u WHERE u.id = :userId")
-	 * void removeUser(@Param("userId") Integer userId);
-	 *
-	 * @Modifying
-	 *
-	 * @Query("update Student s set s.studentNumber = :studentNumber where s.id = :userId"
-	 * )
-	 * void updateStudentNumber(@Param("userId") Integer userId, @Param
-	 * ("studentNumber") Integer studentNumber);
-	 *
-	 * @Modifying
-	 *
-	 * @Query("update Student s set s.masters = :masters where s.id = :userId")
-	 * void updateStudentMasters(@Param("userId") Integer userId, @Param ("masters")
-	 * Masters masters);
-	 *
-	 * @Modifying
-	 *
-	 * @Query("update Student s set s.averageGrade = :averageGrade where s.id = :userId"
-	 * )
-	 * void updateStudentGrades(@Param("userId") Integer userId, @Param
-	 * ("averageGrade") Float averageGrade);
-	 *
-	 * @Modifying
-	 *
-	 * @Query("update Professor p set p.emailField = :emailField where p.id = :userId"
-	 * )
-	 * void updateProfessorEmail(@Param("userId") Integer userId, @Param
-	 * ("emailField") String emailField);
-	 *
-	 * @Modifying
-	 *
-	 * @Query("update Consultant c set c.company = :company where c.id = :userId")
-	 * void updateConsultantCompany(@Param("userId") Integer userId, @Param
-	 * ("company") String company);
-	 */
+	@Modifying
+
+	@Query("update Student s set s.masters = :masters where s.id = :userId")
+	void updateStudentMasters(@Param("userId") Integer userId, @Param ("masters") Masters masters);
+
+	@Modifying
+
+	@Query("update Student s set s.averageGrade = :averageGrade where s.id = :userId"
+	 )
+	void updateStudentGrades(@Param("userId") Integer userId, @Param
+	 ("averageGrade") Float averageGrade);
+
+	@Modifying
+
+	@Query("update Professor p set p.emailField = :emailField where p.id = :userId"
+	 )
+	void updateProfessorEmail(@Param("userId") Integer userId, @Param
+	 ("emailField") String emailField);
+
+	@Modifying
+
+	@Query("update Consultant c set c.company = :company where c.id = :userId")
+	 void updateConsultantCompany(@Param("userId") Integer userId, @Param ("company") String company); */
+
 }
