7d6a1da2509115750903fe433406b29c6f4a5396
fc56380 <fc56380@alunos.fc.ul.pt>
2024-04-02 22:09:49 +0100

    added tests

--- src/test/java/pt/ul/fc/css/example/demo/DemoApplicationTests.java
+++ src/test/java/pt/ul/fc/css/example/demo/DemoApplicationTests.java
@@ -1,59 +1,74 @@
 package pt.ul.fc.css.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import pt.ul.fc.css.example.demo.entities.DissertationTopic;
+import pt.ul.fc.css.example.demo.entities.AppUser;
+import pt.ul.fc.css.example.demo.repositories.ApplicationRepository;
+import pt.ul.fc.css.example.demo.repositories.DefenseRepository;
 import pt.ul.fc.css.example.demo.repositories.DissertationTopicRepository;
 import pt.ul.fc.css.example.demo.repositories.MastersRepository;
+import pt.ul.fc.css.example.demo.repositories.ThesisExecutionRepository;
 import pt.ul.fc.css.example.demo.repositories.UserRepository;
 
 @SpringBootTest
 class DemoApplicationTests {
 
   @Autowired private UserRepository userRepository;
-  //@Autowired private MastersRepository mastersRepository;
+  @Autowired private MastersRepository mastersRepository;
   @Autowired private DissertationTopicRepository dissertationTopicRepository;
+  @Autowired private ThesisExecutionRepository thesisExecutionRepository;
+  @Autowired private ApplicationRepository ApplicationRepository;
+  @Autowired private DefenseRepository defenseRepository;
 
   @Test
   void testUserListIsNotEmpty() {
     assertTrue(userRepository.count() > 0);
   }
-/** 
+
   @Test
   void testMastersListIsNotEmpty() {
     assertTrue(mastersRepository.count() > 0);
   }
-  */
-/** 
-  @Test
-  void testUserName() {
-    // Masters master = mastersRepository.findByName("Matematica").get(0);
-    // AppUser professor = userRepository.findByName("profname1").get(0);
-    // assertEquals(master.getCoordinator(), professor);
-    assertTrue(true);
-  }
-  */
- 
+
   @Test
   void testTopicListIsNotEmpty() {
-    assertTrue(dissertationTopicRepository.count() > 0);
-  }
-  
-  @Test
-  void testDescription() {
-    assertEquals("Memo bom", dissertationTopicRepository.findByTitle("TREX").get(0).getDescription());
+	assertTrue(dissertationTopicRepository.count() > 0);
   }
 
   @Test
-  void testSize() {
-    assertEquals(1, dissertationTopicRepository.findByTitle("Biologia").size());
+  void tesisListIsNotEmpty() {
+	assertTrue(thesisExecutionRepository.count() > 0);
   }
-  
 
+  @Test
+  void ApplicationListIsNotEmpty() {
+	assertTrue(ApplicationRepository.count() > 0);
+  }
+
+  @Test
+  void DefenseListIsNotEmpty() {
+	assertTrue(defenseRepository.count() > 0);
+  }
+
+
+  @Test
+  void testFindByName() {
+    AppUser professor = userRepository.findByName("cristiano ronaldo").get(0);
+    assertEquals(professor.getName(), "cristiano ronaldo");
+  }
+
+  void testFindByUserName() {
+    AppUser professor = userRepository.findByUserName("cr7").get(0);
+    assertEquals(professor.getName(), "cr7");
+  }
+
+  void testFindByStudentNumber() {
+    AppUser professor = userRepository.findByUserName("cr7").get(0);
+    assertEquals(professor.getName(), "cr7");
+  }
 }
