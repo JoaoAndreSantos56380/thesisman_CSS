6ed83eee523c72e25a918bb51fe6018a2999c15e
fc57544 <rafaelmmf19062000@gmail.com>
2024-04-03 17:12:13 +0100

    Removed Cascade due to causing bugsand errors

--- src/main/java/pt/ul/fc/css/example/demo/entities/ThesisExecution.java
+++ src/main/java/pt/ul/fc/css/example/demo/entities/ThesisExecution.java
@@ -18,20 +18,21 @@ public class ThesisExecution {
 
   // associacao one-to-one
   @NonNull
-  @OneToOne // (cascade = CascadeType.ALL)
+  @OneToOne
   @JoinColumn(name = "fk_student_id", referencedColumnName = "id")
   private Student student;
 
   @NonNull
-  @OneToOne // (cascade = CascadeType.ALL)
+  @OneToOne
   @JoinColumn(name = "fk_dissertation_topic_id", referencedColumnName = "id")
   private DissertationTopic topic;
 
   @NonNull
-  @ManyToOne // (cascade = CascadeType.PERSIST)
+  @ManyToOne
   @JoinColumn(name = "fk_internal_advisor_id", referencedColumnName = "id")
   private Professor internalAdvisor;
 
+  
   @NonNull private String yearOfExecution;
 
   @Nullable private int finalGrade;
