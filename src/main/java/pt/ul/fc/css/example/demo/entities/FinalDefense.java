bea5b6b84ae2056e0f6ea4cd4c8c28eb0b69b76c
JPBotelho <jpbotelho.costa@gmail.com>
2024-04-03 11:18:43 +0100

    FinalDefense: removed finalGrade from constructor, added default constructor

--- src/main/java/pt/ul/fc/css/example/demo/entities/FinalDefense.java
+++ src/main/java/pt/ul/fc/css/example/demo/entities/FinalDefense.java
@@ -1,23 +1,23 @@
 package pt.ul.fc.css.example.demo.entities;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-
 import java.util.Date;
-
 import org.springframework.lang.NonNull;
 
 @Entity
 public class FinalDefense extends ThesisDefense {
   // associacao many-to-one
   @NonNull
-  @ManyToOne //(cascade = CascadeType.ALL)
+  @ManyToOne // (cascade = CascadeType.ALL)
   @JoinColumn(name = "fk_president_id", referencedColumnName = "id")
   private Professor president;
 
-  public FinalDefense (ThesisExecution thesisExecution, String location, Date time, double grade, Professor president){
-	super(thesisExecution, location, time, grade);
+  public FinalDefense(
+      ThesisExecution thesisExecution, String location, Date time, Professor president) {
+    super(thesisExecution, location, time);
   }
+
+  public FinalDefense() {}
 }
