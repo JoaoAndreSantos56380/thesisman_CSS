31c9b8c48609fb754932ac54a83931e5d5da8dcd
fc56380 <fc56380@alunos.fc.ul.pt>
2024-05-12 10:41:55 +0100

    added exceptions

---
+++ src/main/java/pt/ul/fc/css/example/demo/services/EmptyFieldsException.java
@@ -0,0 +1,9 @@
+package pt.ul.fc.css.example.demo.services;
+
+public class EmptyFieldsException extends ApplicationException {
+
+    public EmptyFieldsException(String message) {
+        super(message);
+    }
+
+}
