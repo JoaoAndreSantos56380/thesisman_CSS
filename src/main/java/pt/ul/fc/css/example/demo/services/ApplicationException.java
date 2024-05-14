477cd2eadf5929a3fb381dee57ff8ca0c9872bf1
fc56380 <fc56380@alunos.fc.ul.pt>
2024-05-14 16:37:38 +0100

    deleted unused files

--- src/main/java/pt/ul/fc/css/example/demo/services/ApplicationException.java
+++
@@ -1,7 +0,0 @@
-package pt.ul.fc.css.example.demo.services;
-
-public class ApplicationException extends Exception {
-    public ApplicationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/pt/ul/fc/css/example/demo/services/EmptyFieldsException.java
+++
@@ -1,9 +0,0 @@
-package pt.ul.fc.css.example.demo.services;
-
-public class EmptyFieldsException extends ApplicationException {
-
-    public EmptyFieldsException(String message) {
-        super(message);
-    }
-
-}
