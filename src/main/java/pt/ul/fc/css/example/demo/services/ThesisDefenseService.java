ccc4b7b9e9995366775ddcd18b760181667cb087
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-16 17:15:11 +0100

    added methods related to statistics

--- src/main/java/pt/ul/fc/css/example/demo/services/ThesisDefenseService.java
+++ src/main/java/pt/ul/fc/css/example/demo/services/ThesisDefenseService.java
@@ -3,23 +3,33 @@ package pt.ul.fc.css.example.demo.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import pt.ul.fc.css.example.demo.entities.Masters;
 import pt.ul.fc.css.example.demo.entities.ThesisDefense;
 import pt.ul.fc.css.example.demo.entities.ThesisExecution;
+import pt.ul.fc.css.example.demo.handlers.StatisticsHandlerP;
 import pt.ul.fc.css.example.demo.repositories.DefenseRepository;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 @Component
 public class ThesisDefenseService {
-	@Autowired
-	private DefenseRepository defenseRepository;
+    @Autowired
+    private DefenseRepository defenseRepository;
 
-	public List<ThesisDefense> getAllPersons() {
-		return defenseRepository.findAll();
-	}
+    @Autowired
+    private StatisticsHandlerP statsHandler;
 
-	public ThesisDefense addDefense(ThesisExecution te, String location, Date time) {
-		return defenseRepository.save(new ThesisDefense(te, location, time));
-	}
+    public List<ThesisDefense> findAllDefenses() {
+        return statsHandler.findAllDefenses();
+    }
+
+    public ThesisDefense addDefense(ThesisExecution te, String location, Date time) {
+        return defenseRepository.save(new ThesisDefense(te, location, time));
+    }
+
+    public List<ThesisDefense> findAllPositives() {
+        return statsHandler.findAllPositives();
+    }
 }
