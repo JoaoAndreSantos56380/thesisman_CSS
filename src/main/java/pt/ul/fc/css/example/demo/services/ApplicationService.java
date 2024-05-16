86fa3b569c5a470f153817c7414ad3e87056b477
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-16 13:39:37 +0100

    implemented some methods

---
+++ src/main/java/pt/ul/fc/css/example/demo/services/ApplicationService.java
@@ -0,0 +1,30 @@
+package pt.ul.fc.css.example.demo.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pt.ul.fc.css.example.demo.entities.Application;
+import pt.ul.fc.css.example.demo.entities.DissertationTopic;
+import pt.ul.fc.css.example.demo.entities.Student;
+import pt.ul.fc.css.example.demo.repositories.ApplicationRepository;
+
+@Service
+public class ApplicationService {
+
+    @Autowired
+    private ApplicationRepository applicationRepository;
+
+    public Application addApplication(Application application) {
+        return applicationRepository.save(application);
+    }
+
+    public List<Application> findApplicationsByStudent(Student student) {
+        return applicationRepository.findByStudent(student);
+    }
+
+    public List<Application> findApplicationsByTopic(DissertationTopic topic) {
+        return applicationRepository.findByTopic(topic);
+    }
+
+}
+
