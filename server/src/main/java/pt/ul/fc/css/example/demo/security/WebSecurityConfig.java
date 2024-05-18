d88453949d51ad64ad87a08dc9524a6e36f9c64c
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-18 13:36:10 +0100

    Doesnt require login to access API

--- server/src/main/java/pt/ul/fc/css/example/demo/security/WebSecurityConfig.java
+++ server/src/main/java/pt/ul/fc/css/example/demo/security/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 		http.authorizeHttpRequests((requests) -> requests
-						.requestMatchers("/consultant/register", "/")
+						.requestMatchers("/consultant/register", "/", "/api/**")
 						.permitAll()
 						.anyRequest().authenticated())
 				.formLogin((form) -> form
