64287247ebd40b0a3d873f252daa6b51da95ced6
JPBotelho <jpbotelho.costa@gmail.com>
2024-05-20 16:46:24 +0100

    JAVAFX: Reused Execution parser

--- client/src/main/java/pt/ul/fc/di/css/javafxexample/presentation/model/APIParser.java
+++ client/src/main/java/pt/ul/fc/di/css/javafxexample/presentation/model/APIParser.java
@@ -178,110 +178,14 @@ public class APIParser {
         try {
             // Parse the JSON response
             JsonArray thesisExecutionsArray = JsonParser.parseString(content).getAsJsonArray();
-          
+
             // Create a list to hold the ThesisExecutionModel instances
             List<ThesisExecutionModel> thesisExecutions = new ArrayList<>();
-            
+
             // Process each thesis execution
-            for (JsonElement thesisElement : thesisExecutionsArray) {
-                JsonObject thesisObject = thesisElement.getAsJsonObject();
-                
-                long id = thesisObject.get("id").getAsLong();
-                
-                // Parse student
-                JsonObject studentObject = thesisObject.getAsJsonObject("student");
-                String studentId = studentObject.get("id").getAsString();
-                String studentUsername = studentObject.get("username").getAsString();
-                String studentPassword = studentObject.get("password").getAsString();
-                String studentName = studentObject.get("name").getAsString();
-                int studentNumber = studentObject.get("studentNumber").getAsInt();
-                double averageGrade = studentObject.get("averageGrade").getAsDouble();
-                
-                // Parse student's masters
-                JsonObject masterObject = studentObject.getAsJsonObject("masters");
-                long masterId = masterObject.get("id").getAsLong();
-                String masterName = masterObject.get("name").getAsString();
-                
-                // Parse master's coordinator
-                JsonObject coordinatorObject = masterObject.getAsJsonObject("coordinator");
-                long coordinatorId = coordinatorObject.get("id").getAsLong();
-                String coordinatorUsername = coordinatorObject.get("username").getAsString();
-                String coordinatorPassword = coordinatorObject.get("password").getAsString();
-                String coordinatorName = coordinatorObject.get("name").getAsString();
-                
-                // Create coordinator model
-                ProfessorModel coordinator = new ProfessorModel(coordinatorUsername, coordinatorPassword, coordinatorName);
-                
-                // Create master's model
-                MastersModel master = new MastersModel(masterName, coordinator);
-                
-                // Create student model
-                StudentModel student = new StudentModel(studentUsername, studentPassword, studentName, studentNumber, averageGrade, null);
-                
-                // Parse topic
-                JsonObject topicObject = thesisObject.getAsJsonObject("topic");
-                long topicId = topicObject.get("id").getAsLong();
-                String topicTitle = topicObject.get("title").getAsString();
-                String topicDescription = topicObject.get("description").getAsString();
-                double topicSalary = topicObject.get("salary").getAsDouble();
-                
-                // Parse submitter
-                JsonObject submitterObject = topicObject.getAsJsonObject("submitter");
-                long submitterId = submitterObject.get("id").getAsLong();
-                String submitterUsername = submitterObject.get("username").getAsString();
-                String submitterPassword = submitterObject.get("password").getAsString();
-                String submitterName = submitterObject.get("name").getAsString();
-                
-                // Create submitter model
-                AppUserModel submitter = new AppUserModel(submitterUsername, submitterPassword, submitterName);
-                
-                // Parse compatible masters
-                Set<MastersModel> compatibleMasters = new HashSet<>();
-                JsonArray mastersArray = topicObject.getAsJsonArray("compatibleMasters");
-                for (JsonElement masterElement : mastersArray) {
-                    JsonObject compatibleMasterObject = masterElement.getAsJsonObject();
-                    long compatibleMasterId = compatibleMasterObject.get("id").getAsLong();
-                    String compatibleMasterName = compatibleMasterObject.get("name").getAsString();
-                    
-                    // Parse coordinator of compatible master
-                    JsonObject compatibleCoordinatorObject = compatibleMasterObject.getAsJsonObject("coordinator");
-                    long compatibleCoordinatorId = compatibleCoordinatorObject.get("id").getAsLong();
-                    String compatibleCoordinatorUsername = compatibleCoordinatorObject.get("username").getAsString();
-                    String compatibleCoordinatorPassword = compatibleCoordinatorObject.get("password").getAsString();
-                    String compatibleCoordinatorName = compatibleCoordinatorObject.get("name").getAsString();
-                    
-                    // Create coordinator model
-                    ProfessorModel compatibleCoordinator = new ProfessorModel(compatibleCoordinatorUsername, compatibleCoordinatorPassword, compatibleCoordinatorName);
-                    
-                    // Create compatible master model
-                    MastersModel compatibleMaster = new MastersModel(compatibleMasterName, compatibleCoordinator);
-                    compatibleMasters.add(compatibleMaster);
-                }
-                
-                // Create dissertation topic model
-                DissertationTopicModel topic = new DissertationTopicModel(topicTitle, topicDescription, topicSalary, submitter, compatibleMasters);
-                topic.setId(topicId);
-                
-                // Parse internal advisor
-                JsonObject internalAdvisorObject = thesisObject.getAsJsonObject("internalAdvisor");
-                long internalAdvisorId = internalAdvisorObject.get("id").getAsLong();
-                String internalAdvisorUsername = internalAdvisorObject.get("username").getAsString();
-                String internalAdvisorPassword = internalAdvisorObject.get("password").getAsString();
-                String internalAdvisorName = internalAdvisorObject.get("name").getAsString();
-                
-                // Create internal advisor model
-                ProfessorModel internalAdvisor = new ProfessorModel(internalAdvisorUsername, internalAdvisorPassword, internalAdvisorName);
-                
-                // Parse year of execution and final grade
-                String yearOfExecution = thesisObject.get("yearOfExecution").getAsString();
-                int finalGrade = thesisObject.get("finalGrade").getAsInt();
-                
-                // Create thesis execution model
-                ThesisExecutionModel thesisExecution = new ThesisExecutionModel(student, topic, yearOfExecution, internalAdvisor);
-                thesisExecution.setId(id);
-                thesisExecution.setFinalGrade(finalGrade);
-                
-                // Add to the list
+            for (JsonElement thesisExecutionElement : thesisExecutionsArray) {
+                JsonObject thesisExecutionObject = thesisExecutionElement.getAsJsonObject();
+                ThesisExecutionModel thesisExecution = parseThesisExecution(thesisExecutionObject);
                 thesisExecutions.add(thesisExecution);
             }
             return thesisExecutions;
@@ -289,5 +193,96 @@ public class APIParser {
             e.printStackTrace();
             return new ArrayList<>();
         }
+    }
+
+    private static ThesisExecutionModel parseThesisExecution(JsonObject thesisExecutionObject) {
+        long id = thesisExecutionObject.get("id").getAsLong();
+        
+        // Parse student
+        JsonObject studentObject = thesisExecutionObject.getAsJsonObject("student");
+        String studentUsername = studentObject.get("username").getAsString();
+        String studentPassword = studentObject.get("password").getAsString();
+        String studentName = studentObject.get("name").getAsString();
+        int studentNumber = studentObject.get("studentNumber").getAsInt();
+        double averageGrade = studentObject.get("averageGrade").getAsDouble();
+        
+        // Parse student's masters
+        JsonObject masterObject = studentObject.getAsJsonObject("masters");
+        String masterName = masterObject.get("name").getAsString();
+        
+        // Parse master's coordinator
+        JsonObject coordinatorObject = masterObject.getAsJsonObject("coordinator");
+        String coordinatorUsername = coordinatorObject.get("username").getAsString();
+        String coordinatorPassword = coordinatorObject.get("password").getAsString();
+        String coordinatorName = coordinatorObject.get("name").getAsString();
+        
+        // Create coordinator model
+        ProfessorModel coordinator = new ProfessorModel(coordinatorUsername, coordinatorPassword, coordinatorName);
+        
+        // Create master's model
+        MastersModel master = new MastersModel(masterName, coordinator);
+        
+        // Create student model
+        StudentModel student = new StudentModel(studentUsername, studentPassword, studentName, studentNumber, averageGrade, null);
+        
+        // Parse topic
+        JsonObject topicObject = thesisExecutionObject.getAsJsonObject("topic");
+        String topicTitle = topicObject.get("title").getAsString();
+        String topicDescription = topicObject.get("description").getAsString();
+        double topicSalary = topicObject.get("salary").getAsDouble();
+        
+        // Parse submitter
+        JsonObject submitterObject = topicObject.getAsJsonObject("submitter");
+        String submitterUsername = submitterObject.get("username").getAsString();
+        String submitterPassword = submitterObject.get("password").getAsString();
+        String submitterName = submitterObject.get("name").getAsString();
+        
+        // Create submitter model
+        AppUserModel submitter = new AppUserModel(submitterUsername, submitterPassword, submitterName);
+        
+        // Parse compatible masters
+        Set<MastersModel> compatibleMasters = new HashSet<>();
+        JsonArray mastersArray = topicObject.getAsJsonArray("compatibleMasters");
+        for (JsonElement masterElement : mastersArray) {
+            JsonObject compatibleMasterObject = masterElement.getAsJsonObject();
+            String compatibleMasterName = compatibleMasterObject.get("name").getAsString();
+            
+            // Parse coordinator of compatible master
+            JsonObject compatibleCoordinatorObject = compatibleMasterObject.getAsJsonObject("coordinator");
+            String compatibleCoordinatorUsername = compatibleCoordinatorObject.get("username").getAsString();
+            String compatibleCoordinatorPassword = compatibleCoordinatorObject.get("password").getAsString();
+            String compatibleCoordinatorName = compatibleCoordinatorObject.get("name").getAsString();
+            
+            // Create coordinator model
+            ProfessorModel compatibleCoordinator = new ProfessorModel(compatibleCoordinatorUsername, compatibleCoordinatorPassword, compatibleCoordinatorName);
+            
+            // Create compatible master model
+            MastersModel compatibleMaster = new MastersModel(compatibleMasterName, compatibleCoordinator);
+            compatibleMasters.add(compatibleMaster);
+        }
+        
+        // Create dissertation topic model
+        DissertationTopicModel topic = new DissertationTopicModel(topicTitle, topicDescription, topicSalary, submitter, compatibleMasters);
+        topic.setId(topicObject.get("id").getAsLong());
+        
+        // Parse internal advisor
+        JsonObject internalAdvisorObject = thesisExecutionObject.getAsJsonObject("internalAdvisor");
+        String internalAdvisorUsername = internalAdvisorObject.get("username").getAsString();
+        String internalAdvisorPassword = internalAdvisorObject.get("password").getAsString();
+        String internalAdvisorName = internalAdvisorObject.get("name").getAsString();
+        
+        // Create internal advisor model
+        ProfessorModel internalAdvisor = new ProfessorModel(internalAdvisorUsername, internalAdvisorPassword, internalAdvisorName);
+        
+        // Parse year of execution and final grade
+        String yearOfExecution = thesisExecutionObject.get("yearOfExecution").getAsString();
+        int finalGrade = thesisExecutionObject.get("finalGrade").getAsInt();
+        
+        // Create thesis execution model
+        ThesisExecutionModel thesisExecution = new ThesisExecutionModel(student, topic, yearOfExecution, internalAdvisor);
+        thesisExecution.setId(id);
+        thesisExecution.setFinalGrade(finalGrade);
+        
+        return thesisExecution;
     }
 }
