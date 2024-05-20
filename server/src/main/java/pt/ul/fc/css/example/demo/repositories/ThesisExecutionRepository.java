bfe76475ff3dea5531e2e4ba37028d0516fe769d
fc56380 <fc56380@alunos.fc.ul.pt>
2024-05-20 16:04:10 +0100

    added new queries for thesisExecution

--- server/src/main/java/pt/ul/fc/css/example/demo/repositories/ThesisExecutionRepository.java
+++ server/src/main/java/pt/ul/fc/css/example/demo/repositories/ThesisExecutionRepository.java
@@ -5,25 +5,39 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import pt.ul.fc.css.example.demo.entities.AppUser;
 import pt.ul.fc.css.example.demo.entities.DissertationTopic;
 import pt.ul.fc.css.example.demo.entities.Professor;
 import pt.ul.fc.css.example.demo.entities.Student;
 import pt.ul.fc.css.example.demo.entities.ThesisExecution;
 
 public interface ThesisExecutionRepository extends JpaRepository<ThesisExecution, Long> {
-  @Query("SELECT t FROM ThesisExecution t WHERE t.student = :student")
-  List<ThesisExecution> findByStudent(@Param("student") Student student);
+	@Query("SELECT t FROM ThesisExecution t WHERE t.student = :student")
+	List<ThesisExecution> findByStudent(@Param("student") Student student);
 
-  @Query("SELECT t FROM ThesisExecution t WHERE t.topic = :topic")
-  List<ThesisExecution> findByDissertationTopic(@Param("topic") DissertationTopic topic);
+	@Query("SELECT t FROM ThesisExecution t WHERE t.topic = :topic")
+	List<ThesisExecution> findByDissertationTopic(@Param("topic") DissertationTopic topic);
 
-  @Query("SELECT t FROM ThesisExecution t WHERE t.yearOfExecution LIKE %:year%")
-  List<ThesisExecution> findByYearOfExecution(@Param("year") String year);
+	@Query("SELECT t FROM ThesisExecution t WHERE t.yearOfExecution LIKE %:year%")
+	List<ThesisExecution> findByYearOfExecution(@Param("year") String year);
 
-  @Query("SELECT t FROM ThesisExecution t WHERE t.finalGrade = :finalGrade")
-  List<ThesisExecution> findByFinalGrade(@Param("finalGrade") Integer finalGrade);
+	@Query("SELECT t FROM ThesisExecution t WHERE t.finalGrade = :finalGrade")
+	List<ThesisExecution> findByFinalGrade(@Param("finalGrade") Integer finalGrade);
 
-  @Query("SELECT t FROM ThesisExecution t WHERE t.internalAdvisor = :internalAdvisor")
-  List<ThesisExecution> findByInternalAdvisor(@Param("internalAdvisor") Professor internalAdvisor);
+	@Query("SELECT t FROM ThesisExecution t WHERE t.internalAdvisor = :internalAdvisor")
+	List<ThesisExecution> findByInternalAdvisor(@Param("internalAdvisor") Professor internalAdvisor);
+
+	@Query("SELECT t FROM ThesisExecution t LEFT JOIN ThesisDefense d ON t.id = d.thesisExecution.id WHERE t.internalAdvisor = :internalAdvisor AND (d.id IS NULL OR (d.id IS NOT NULL AND d.grade < 10 AND d.grade != -1))")
+	List<ThesisExecution> findUnscheduledTheses(@Param("internalAdvisor") Professor internalAdvisor);
+
+	@Query("SELECT te FROM ThesisExecution te LEFT JOIN ThesisDefense td ON te.id = td.thesisExecution.id WHERE (td.grade = -1 AND te.internalAdvisor = :internalAdvisor)")
+	List<ThesisExecution> findScheduledTheses(@Param("internalAdvisor") Professor internalAdvisor);
+
+	//@Query("SELECT te FROM ThesisExecution te LEFT JOIN FinalDefense td ON te.id = td.thesisExecution.id WHERE td.id IS NULL AND te.internalAdvisor = :internalAdvisor")
+	//@Query("SELECT te FROM ThesisExecution te JOIN te.thesisDefenses td WHERE td.grade > 10 AND te.id NOT IN (SELECT fe.thesisExecution.id FROM FinalDefense fe")
+	//@Query("SELECT te FROM ThesisExecution te LEFT JOIN te.thesisDefenses td WHERE td IS NULL OR td.grade < 10")
+	//@Query("SELECT te FROM ThesisExecution te LEFT JOIN FinalDefense fd ON fd.thesisExecution= te WHERE fd.id IS NULL")
+	@Query("SELECT te FROM ThesisExecution te LEFT JOIN FinalDefense fd ON fd.thesisExecution.id = te.id WHERE fd.id IS NULL AND te.internalAdvisor = :internalAdvisor")
+	List<ThesisExecution> findScheduledFinal(@Param("internalAdvisor") Professor internalAdvisor);
 
 }
