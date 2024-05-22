8b5689ac09dc766be5dd04700ba99bd6fbde148b
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-22 18:13:49 +0100

    Added test to compatible masters

--- server/src/test/java/pt/ul/fc/css/example/demo/DissertationTopicRepositoryTests.java
+++ server/src/test/java/pt/ul/fc/css/example/demo/DissertationTopicRepositoryTests.java
@@ -3,9 +3,9 @@ package pt.ul.fc.css.example.demo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/* import java.util.ArrayList; */
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
@@ -14,16 +14,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import pt.ul.fc.css.example.demo.entities.Masters;
 import pt.ul.fc.css.example.demo.entities.Professor;
 
-/* import pt.ul.fc.css.example.demo.entities.Consultant; */
 import pt.ul.fc.css.example.demo.entities.DissertationTopic;
-/* import pt.ul.fc.css.example.demo.entities.Masters;
-import pt.ul.fc.css.example.demo.entities.Professor;
-import pt.ul.fc.css.example.demo.entities.Student; */
-/* import pt.ul.fc.css.example.demo.repositories.ApplicationRepository;
-import pt.ul.fc.css.example.demo.repositories.DefenseRepository; */
 import pt.ul.fc.css.example.demo.repositories.DissertationTopicRepository;
 import pt.ul.fc.css.example.demo.repositories.MastersRepository;
-/* import pt.ul.fc.css.example.demo.repositories.ThesisExecutionRepository; */
 import pt.ul.fc.css.example.demo.repositories.UserRepository;
 
 @Transactional
@@ -33,9 +26,6 @@ class DissertationTopicRepositoryTests {
   @Autowired private UserRepository userRepository;
   @Autowired private MastersRepository mastersRepository;
   @Autowired private DissertationTopicRepository dissertationTopicRepository;
-/*   @Autowired private ThesisExecutionRepository thesisExecutionRepository;
-  @Autowired private ApplicationRepository ApplicationRepository;
-  @Autowired private DefenseRepository defenseRepository; */
 
 
   @Test
@@ -52,7 +42,7 @@ class DissertationTopicRepositoryTests {
     Masters testMaster = new Masters("Test Masters", testCoordinator);
     mastersRepository.save(testMaster);
 
-    assertEquals(1, dissertationTopicRepository.findByTitle("Futebol radiante").size());
+    assertEquals(true, dissertationTopicRepository.findByTitle("Futebol radiante").isPresent()/* .size() */);
   }
 
   @Test
@@ -84,12 +74,14 @@ class DissertationTopicRepositoryTests {
     userRepository.save(test);
     mastersRepository.save(masters);
 
-    List<Masters> testMaster = mastersRepository.findByName("Estudos economicos Europeus");
-
-    DissertationTopic topic = new DissertationTopic("Guatemala studies", "top", 0, test, new HashSet<Masters> (testMaster));
+    Optional<Masters> testMaster = mastersRepository.findByName("Estudos economicos Europeus");
+	assertEquals(true, testMaster.isPresent());
+	HashSet<Masters> set = new HashSet<Masters>();
+	set.add(masters);
+    DissertationTopic topic = new DissertationTopic("Guatemala studies", "top", 0, test, set);
     dissertationTopicRepository.save(topic);
 
-    DissertationTopic testTopic = dissertationTopicRepository.findByCompatibleMasters(testMaster.get(0)).get(0);
+    DissertationTopic testTopic = dissertationTopicRepository.findByCompatibleMasters(testMaster.get()).get(0);
 
     assertEquals("top", testTopic.getDescription());
   }
