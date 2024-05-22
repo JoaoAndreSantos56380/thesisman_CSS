55683c97af86857e2192156175f62565ab1e680f
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-22 18:25:29 +0100

    Optional bug fix

--- server/src/main/java/pt/ul/fc/css/example/demo/repositories/DissertationTopicRepository.java
+++ server/src/main/java/pt/ul/fc/css/example/demo/repositories/DissertationTopicRepository.java
@@ -1,6 +1,8 @@
 package pt.ul.fc.css.example.demo.repositories;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,15 +12,15 @@ import pt.ul.fc.css.example.demo.entities.Masters;
 
 public interface DissertationTopicRepository extends JpaRepository<DissertationTopic, Long> {
 
-	@Query("SELECT d FROM DissertationTopic d WHERE d.submitter = :submitter")
-	List<DissertationTopic> findBySubmitter(@Param("submitter") AppUser submitter);
+    @Query("SELECT d FROM DissertationTopic d WHERE d.submitter = :submitter")
+    List<DissertationTopic> findBySubmitter(@Param("submitter") AppUser submitter);
 
-	@Query("SELECT d FROM DissertationTopic d JOIN d.compatibleMasters m WHERE m = :masters")
-	List<DissertationTopic> findByCompatibleMasters(@Param("masters") Masters masters);
+    @Query("SELECT d FROM DissertationTopic d JOIN d.compatibleMasters m WHERE m = :masters")
+    List<DissertationTopic> findByCompatibleMasters(@Param("masters") Masters masters);
 
-	@Query("SELECT dt FROM DissertationTopic dt WHERE dt.title LIKE %:q% ")
-	List<DissertationTopic> findByTitle(@Param("q") String q);
+    @Query("SELECT dt FROM DissertationTopic dt WHERE dt.title LIKE %:q% ")
+    Optional<DissertationTopic> findByTitle(@Param("q") String q);
 
-	@Query("SELECT dt FROM DissertationTopic dt LEFT JOIN ThesisExecution te ON dt.id = te.topic.id JOIN dt.compatibleMasters cm WHERE te.id IS NULL AND cm = :masters")
-	List<DissertationTopic> findFreeTopics(@Param("masters") Masters masters);
+    @Query("SELECT dt FROM DissertationTopic dt LEFT JOIN ThesisExecution te ON dt.id = te.topic.id JOIN dt.compatibleMasters cm WHERE te.id IS NULL AND cm = :masters")
+    List<DissertationTopic> findFreeTopics(@Param("masters") Masters masters);
 }
